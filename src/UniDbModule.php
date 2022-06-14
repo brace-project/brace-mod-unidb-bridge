97536ec17add7d789db896ddff36e548ed1545d2
Matthias <m@tth.es>
2022-06-14 13:29:55 +0200

    init

--- src/UniDbModule.php
+++ src/UniDbModule.php
@@ -2,7 +2,36 @@
 
 namespace Brace\UniDbBridge;
 
-class UniDbModule
-{
+use Brace\Core\BraceApp;
+use Brace\Core\BraceModule;
+use Phore\Di\Container\Producer\DiService;
+use Phore\UniDb\UniDb;
 
+class UniDbModule implements BraceModule
+{
+    public function __construct(
+        /**
+         * @var array<string, UniDb>
+         */
+        private array $connections
+    )
+    {
+
+
+    }
+
+
+    public function register(BraceApp $app)
+    {
+        foreach($this->connections as $key => $con) {
+            $app->define($key, new DiService($con));
+        }
+
+        $app->command->addCommand("create_schema", function () use ($app) {
+            foreach ($this->connections as $key => $connection) {
+                $con = $app->get($key, UniDb::class);
+                $con->createSchema();
+            }
+        });
+    }
 }
